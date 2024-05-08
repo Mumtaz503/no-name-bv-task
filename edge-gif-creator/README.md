db40a81773b088004e41e0182fed23654dd99da8
Mumtaz Ahmad <hasaanahmad059@gmail.com>
2024-05-08 10:39:14 +0000

    completed Edge-Gif-Generator

--- edge-gif-creator/README.md
+++ edge-gif-creator/README.md
@@ -1,6 +1,57 @@
-## Memory Management
+# Node GIF creator script
 
-1. Lowering the resolution of the gif
-2. Adjusting the CRF
-3. Lowering the Framerate
+The script `index.js` takes input of a video file in 'MP4' format, a starting timestamp and an ending timestamp.
+It converts the video file in a gif with Edge Video logo added to the bottom right.
 
+## Usage
+
+First of all make sure that you have node and npm installed.
+
+Then you need to install ffmpeg command-line tool on your local machine.
+[learn more](https://ffmpeg.org/)
+
+After installing ffmpeg clone this repository and initialize a node project and install all the dependencies by using
+
+`npm install`
+
+Then you need to run the script as follows:
+
+`node index.js <video file name> <start timestamp in seconds> <end timestamp in seconds>`
+
+Example:
+
+`node index.js video/cats.mp4 12 20`
+
+## Memory and process speed management
+
+I used four methods to minimize memory usage and maximize process speed. However, you can change the `ffmpeg` command to your requirements. The usage of these methods depends heavily on your requirements and use-case.
+
+### Limiting GIF length to 10 seconds
+
+The first method I used was to limit the length of the GIF to 10 seconds. For even better memory management and a better process speed you can lower this number here:
+
+```if (Math.abs(endTime - startTime) > 10) { console.error("The GIF must not be longer than 10 seconds"); process.exit(1); }```
+
+Drawback here is that you may end-up with a very short gif. Again this depends on your use-case.
+
+### Lowering the GIF resolution
+
+The second method I use was to lower the GIF's resolution down to 380 pixels in my `ffmpeg` command. A lowered resolution directly results in a bette memory management and effective process speed. You can change the resolution here at line '26'
+
+```...-filter_complex "[0:v]scale=380:-1[trimmed_video]...```
+
+Drawback here is that you may end-up with an ugly looking GIF if you lower the resolution too much.
+
+### CRF adjustment
+
+It may not have a lot of effect on short GIFs but adjusting the "CRF" or Constant Rate Factor. CRF is a quality-based encoding method that adjusts the compression to maintain a consistent level of perceived quality. A higher CRF value results in lower quality and smaller file size. You can adjust CRF here in line '26'
+
+```...-crf 25...```
+
+### Lowering the Framerate
+
+I also lowered the framerate of the GIF to 12 frames-per-second, you can also adjust it as you like, it depends on your requirements and Use-cases. Framerate is adjusted here at line 26
+
+```...-r 12...```
+
+Drawback with lower framerate is that you may end-up with a low quality GIF. Again this depends on your requirements.
--- edge-gif-creator/index.js
+++ edge-gif-creator/index.js
@@ -2,13 +2,30 @@ const fs = require("fs");
 const { exec } = require("child_process");
 const path = require("path");
 
+/**
+ * This function creates a GIF from a given mp4 video file
+ * @param inputMp4 The input file in ".mp4" format
+ * @param startTimestamp The starting timestamp from which the gif will be generated
+ * @param endTimestamp The ending timestamp at which the gif stops
+ *
+ * @dev The function uses the ffmpeg CL tool to process multimedia. The ffmpeg command is created and then executed
+ * asynchronously using 'exec' function from child_processes module.
+ * The function is optimized for minimal memory usage and speed. Consult README.md in the package.
+ */
 function createGif(inputMp4, startTimestamp, endTimestamp) {
   const outputFolder = path.join(__dirname, "gif");
+
+  //Random number is generated and alloted to the GIF file to prevent file name clashes
   const randomGifNo = Math.floor(Math.random() * 90000) + 10000;
   const outputFileName = path.join(outputFolder, `GIF_${randomGifNo}.gif`);
+
+  //Accessing the logo
   const logoPath = path.join(__dirname, "logo", "logo.jpg");
+
+  //The ffmpeg command to trim the video between timestamps adds a logo on top at defined width and height and converts it into a GIF. (open README.MD for more info)
   const command = `ffmpeg -ss ${startTimestamp} -to ${endTimestamp} -i ${inputMp4} -i ${logoPath} -filter_complex "[0:v]scale=380:-1[trimmed_video];[trimmed_video][1:v]overlay=W-w-10:H-h-10" -crf 25 -r 12 -pix_fmt yuv420p "${outputFileName}"`;
 
+  //The exec function of child_processes module executes the command and calls a 'callback' function to handle the execution results
   exec(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error creating GIF: ${error}`);
@@ -21,8 +38,10 @@ function createGif(inputMp4, startTimestamp, endTimestamp) {
   });
 }
 
+//Command line args
 const args = process.argv.slice(2);
 
+//Checks if the CL arguments are of appropriate length
 if (args.length != 3) {
   console.log(
     "Please enter the following: <input_file> <start timestamp in seconds> <end timestamp in seconds>"
@@ -30,22 +49,38 @@ if (args.length != 3) {
   process.exit(1);
 }
 
+//Defines the arguments
 const inputFile = args[0];
 const startTime = args[1];
 const endTime = args[2];
 
+//Checks if the input file exists
 if (!fs.existsSync(inputFile)) {
   console.error(`file: ${inputFile} doesn't exist`);
   process.exit(1);
 }
 
+//Checks if the input file is in mp4 format or not.
+if (!inputFile.toLowerCase().endsWith(".mp4")) {
+  console.error("Input file must be in MP4 format");
+  process.exit(1);
+}
+
+//Checks if the user has entered valid timestamps
 if (isNaN(startTime) || isNaN(endTime)) {
   console.error("Please enter valid starting and ending timestamps");
   process.exit(1);
 }
 
+//Checks if the timestamps are appropriate
 if (startTime >= endTime) {
   console.error("End timestamp has to be greater than start timestamp");
+  process.exit(1);
+}
+
+//Check for GIF length
+if (Math.abs(endTime - startTime) > 10) {
+  console.error("The GIF must not be longer than 10 seconds");
   process.exit(1);
 }
 
