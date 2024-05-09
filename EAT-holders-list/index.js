94a56652f8dba2716237ab8c63c7ae7757feb8f6
Mumtaz Ahmad <hasaanahmad059@gmail.com>
2024-05-09 16:31:37 +0000

    Generated a token holders list

--- EAT-holders-list/index.js
+++ EAT-holders-list/index.js
@@ -2,6 +2,7 @@ const polygonscan = require("polygonscan-api").init(
   "HWAAYDDKS8JYZUHTSNVQ2PRU3F6SDVNYM1"
 );
 require("dotenv").config();
+const fs = require("fs");
 
 const eatAddress = "0x7C58D971A5dAbd46BC85e81fDAE87b511431452E";
 
@@ -12,7 +13,7 @@ async function fetchEATTransactions(contractAddress) {
       1,
       "latest",
       1,
-      10000,
+      10000, //Fetches 10000 latest transactions, Can't go above it because polygonscan API doesn't allow it
       "desc"
     );
     if (response.status === "1") {
@@ -22,18 +23,27 @@ async function fetchEATTransactions(contractAddress) {
 
       transactions.forEach((transaction) => {
         if (transaction.input.startsWith("0xa9059cbb")) {
-          //ERC20 transfer signature hash
+          //ERC20 transfer signature hash always starts with 0xa9059cbb
           const sender = transaction.from;
           const recipient = "0x" + transaction.input.slice(34, 74);
-          tokenHolders.add(sender);
-          tokenHolders.add(recipient);
+
+          if (!tokenHolders.has(sender)) {
+            tokenHolders.add(sender);
+          }
+          if (!tokenHolders.has(recipient)) {
+            tokenHolders.add(recipient);
+          }
         }
       });
-
       const tokenHolderAddresses = Array.from(tokenHolders);
-      console.log(tokenHolderAddresses); //This logs addresses of token holders resulting from token
-      // transfers for transfers from 1-10000. Need to sort this data to exclude deployer and make
-      // recurcive calls to polygonscan API to fetch all transaction data.
+      console.log("Token holder array length: ",tokenHolderAddresses.length);
+      fs.writeFile("Toke_Holders.txt", tokenHolderAddresses.join("\n"), (e) => {
+        if (e) {
+          console.error("error writing file: ", e);
+        } else {
+          console.log("token holder addresses successfuly written");
+        }
+      });
     } else {
       console.error("Error:", response.message);
     }
@@ -42,4 +52,23 @@ async function fetchEATTransactions(contractAddress) {
   }
 }
 
+// const eatDeployerAddress = async (contractAddress) => {
+//   try {
+//     const response = await polygonscan.contract.getcontractcreation(
+//       contractAddress
+//     );
+
+//     if (response.status === "1" && response.result.length > 0) {
+//       const deployerAddress = response.result[0].contractCreator;
+//       console.log("Deployer Address:", deployerAddress);
+//       return deployerAddress;
+//     } else {
+//       console.error("Error:", response.message);
+//       return null;
+//     }
+//   } catch (error) {
+//     console.error("Error fetching deployer address:", error);
+//     return null;
+//   }
+// };
 fetchEATTransactions(eatAddress);
